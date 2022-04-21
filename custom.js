5fdb57a06ad3272a9a761c0c5c27e11a2ee0b846
unknown <ejupikrasniqi@gmail.com>
2022-04-21 16:27:32 +0200

    Pjeserisht responsive

---
+++ custom.js
@@ -0,0 +1,11 @@
+
+
+let navButton = document.querySelector(".mobile-nav");
+let navHeader = document.querySelector("#header");
+
+console.log(navButton);
+console.log(navHeader);
+
+navButton.addEventListener("click", function () {
+  navHeader.classList.toggle("open-nav");
+});
